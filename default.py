5451654ad78a12b910e5f490091bd6cc8cd9eab9
Alexis BRENON <brenon.alexis@gmail.com>
2015-02-02 18:25:17 +0100

    Catching every exception to ensure an efficient debug Need to do some blocking readings to reduce CPU consumption

--- default.py
+++ default.py
@@ -1,20 +1,40 @@
-import sys
+"""
+Main module for the PythonWrapper XBMC/Kodi script
+"""
 import os
-import xbmc
+try:
+    import xbmc
+except ImportError:
+    LOG = print
+else:
+    LOG = xbmc.log
 
-def main(argc, argv):
-    os.mkdir("/tmp/xbmcwrapper")
-    os.mkfifo("/tmp/xbmcwrapper")
-    pipe = open("/tmp/xbmcwrapper/input_cmd", 'r')
-    output = open("/tmp/xbmcwrapper/output", 'w')
+def main():
+    """ main """
+    LOG("### Python wrapper ### Hello !")
+    try:
+        os.mkdir("/tmp/xbmcwrapper")
+        os.mkfifo("/tmp/xbmcwrapper/input_cmd")
+    except FileExistsError:
+        pass
 
-    while True:
-        line = pipe.readline()
-        if line == "exit\n":
-            break
-        else:
-            output.write(eval(line))
+    with open("/tmp/xbmcwrapper/input_cmd", 'r') as pipe:
+        with open("/tmp/xbmcwrapper/output", 'wa') as output:
+            LOG("### Python wrapper ### File opened !")
+            while True:
+                line = pipe.readline()
+                if line == "exit\n":
+                    break
+                elif len(line) > 0:
+                    LOG("### Python wrapper ### {0}".format(line))
+                    try:
+                        returned = eval(line)
+                    except Exception as eval_exception:
+                        returned = "Exception raised during evaluation : {0}".format(eval_exception)
+                    output.write("Input : < {0} >\n\tReturned : < {1} >\n\n".format(line.strip(),
+                        returned))
+                    output.flush()
     return 0
 
 if __name__ == "__main__":
-    main(len(sys.argv), sys.argv)
+    main()
