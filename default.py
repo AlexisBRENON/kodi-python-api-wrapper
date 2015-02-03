84bf3c9e173ff6b63e69e784284e22961e8d8c9c
Alexis BRENON <brenon.alexis@gmail.com>
2015-02-03 18:32:52 +0100

    No more polling, and allow multi-line execution Need to find how to output to the output file instead of stdout when exec()

--- default.py
+++ default.py
@@ -14,26 +14,38 @@ def main():
     LOG("### Python wrapper ### Hello !")
     try:
         os.mkdir("/tmp/xbmcwrapper")
+    except FileExistsError:
+        pass
+    try:
         os.mkfifo("/tmp/xbmcwrapper/input_cmd")
     except FileExistsError:
         pass
 
-    with open("/tmp/xbmcwrapper/input_cmd", 'r') as pipe:
-        with open("/tmp/xbmcwrapper/output", 'wa') as output:
-            LOG("### Python wrapper ### File opened !")
-            while True:
-                line = pipe.readline()
-                if line == "exit\n":
-                    break
-                elif len(line) > 0:
-                    LOG("### Python wrapper ### {0}".format(line))
+    abort = False
+    with open("/tmp/xbmcwrapper/output", 'a') as output:
+        LOG("### Python wrapper ### File opened !")
+        while not abort:
+            pipe = open("/tmp/xbmcwrapper/input_cmd", "r")
+            cmd = ''.join(pipe.readlines())
+            pipe.close()
+            if cmd == "exit\n":
+                LOG("### Python wrapper ### Exiting")
+                abort = True
+            else:
+                if cmd.startswith("eval\n"):
+                    cmd = cmd.replace("eval\n", "", 1)
+                    exec(cmd)
+                else:
+                    LOG("### Python wrapper ### {0}".format(cmd.strip()))
                     try:
-                        returned = eval(line)
+                        returned = eval(cmd, {"sys.stderr" : output})
                     except Exception as eval_exception:
                         returned = "Exception raised during evaluation : {0}".format(eval_exception)
-                    output.write("Input : < {0} >\n\tReturned : < {1} >\n\n".format(line.strip(),
+                    output.write("Input : < {0} >\n\tReturned : < {1} >\n\n".format(cmd.strip(),
                         returned))
                     output.flush()
+
+    os.unlink("/tmp/xbmcwrapper/input_cmd")
     return 0
 
 if __name__ == "__main__":
